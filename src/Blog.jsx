47f1ee5d7a56ef769aa44f1bb484f81a6c372293
dima sgibnev <dimasgibnevgmai.com>
2024-06-15 19:15:31 +0700

    work on routing

---
+++ src/Blog.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { Content, Footer, Header } from './components';
+
+export const Blog = () => {
+	return (
+	<>
+	<Header/>
+	<Content>
+		<h2>Заголовок</h2>
+	</Content>
+	<Footer/>
+	</>
+	);
+};
---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+export const Content = ({children}) => {
+  return (
+	<>
+	{children}
+	</>
+  )
+}
