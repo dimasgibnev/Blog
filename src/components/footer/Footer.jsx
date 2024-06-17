9fb30fcf868ab6de8de09ac991ce2d2e0ee91a2b
dima sgibnev <dimasgibnev@gmail.com>
2024-06-17 10:42:08 +0700

    realize footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,29 @@
 import styled from 'styled-components';
+import { WeatherWidget } from './components/WeatherWidget';
 
-const Foot = styled.div`
-		display: flex;
+const Description = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+const FooterContainer = ({ className }) => {
+	return (
+		<footer className={className}>
+			<Description>
+				<span>Блог веб-разработчика</span>
+				<span>web-developer.com</span>
+			</Description>
+			<WeatherWidget />
+		</footer>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
 	justify-content: space-between;
 	width: 100%;
 	height: 120px;
 	padding: 20px 40px;
-`
-
-export const Footer = () => {
-	return <Foot>Footer</Foot>;
-};
+	box-shadow: 0 2px 10px #000;
+		font-weight: bold;
+`;
